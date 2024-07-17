01f273a6410df87991aa8018da494f7eb57ddd0b
user-49 <kapil.negi12300@gmail.com>
2024-07-17 18:50:00 +0530

    compiled testing repo

--- Tkinter/project 6.py
+++ Tkinter/project 6.py
@@ -1,6 +1,14 @@
 import tkinter
 from tkinter import ttk
 
-
 window = tkinter.Tk()
-window.geometry('500x600')
+window.title('Buttons')
+window.geometry('500x500')
+
+button_str = tkinter.StringVar()
+simple_button = ttk.Button(master=window,text='this is a simple button',command=lambda:print('you pressed a simple button',textvariable=button_str))
+
+
+simple_button.pack()
+
+window.mainloop()
