35bdbe6420b0df3f460b405ca6eacf34783b8605
user-49 <kapil.negi12300@gmail.com>
2024-07-23 09:56:43 +0530

    update

--- C/project 1.c
+++ C/project 1.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
-int main(){
-    printf("hello world");
-    return 0;
+
+void main(){
+    int principal, year;
+    float simple_intrest, roi;
+
+    printf("Enter principal value: ");
+    scanf("%d", &principal);
+    printf("Enter roi per annum: ");
+    scanf("%f", roi);
+    printf("Enter number of years: ");
+    scanf("%f", &year);
+
+    printf("simple intrestis: %f", principal*roi*year/100);
 }
